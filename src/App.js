68330046c5834ab147ef731829efdd0c7907a4a9
Abu Sufian <ynor7005326@gmail.com>
2020-02-06 16:02:26 +0600

    react factorial application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState(0)
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setResult(getFactorialNumber(event.target[0].value))
+  };
+  const getFactorialNumber = (factorialNumber) => {
+    if (factorialNumber < 0)
+          return -1;
+    else if (factorialNumber == 0)
+        return 1;
+    else {
+        return (factorialNumber * getFactorialNumber(factorialNumber - 1));
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h2>Factorial Calculator</h2>
+        <form className="myForm" onSubmit={handleSubmit}>
+          <input
+              type="number"
+              placeholder="Enter a number..."
+              className="formField"
+              required
+          />
+          <button className="myButton">Calculate Factorial</button>
+        </form>
+        <h2>Factorial: {result}</h2>
+      </div>
   );
 }
 
